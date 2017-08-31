49d3abe0820eb4824e9d100a61c8d246d51e15ba
vikasbz <vikas.zingade@gmail.com>
2017-09-01 02:31:35 +0530

    Update trial4.py

--- trial4.py
+++ trial4.py
@@ -12,10 +12,3 @@ res = urllib.request.urlopen(req)
 print(res.read())
 
 print("hi")
-
-def solution(number):
-    sum = 0
-    for i in range(number):
-        if (i % 3) == 0 or (i % 5) == 0:
-            sum += i
-    return sum
