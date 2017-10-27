aa2674b626e72f070efc1937a289d8be7d32659f
vikasbz <vikas.zingade@gmail.com>
2017-10-28 01:06:31 +0530

    Update nickname_generator.py

--- nickname_generator.py
+++ nickname_generator.py
@@ -1,4 +1,2 @@
 def nickname_generator(name):
-    if len(name) < 4:
-        return 'Error: Name too short' 
-    return name[:4] if name[2] in ('a', 'e', 'i', 'o', 'u') else name[:3] 
+    return 'Error: Name too short' if len(name) < 4 else name[:4] if name[2] in ('a', 'e', 'i', 'o', 'u') else name[:3] 
